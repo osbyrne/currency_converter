8f940b3b68e58f27f98a6e79e9d07d2a164ff22a
Maël AUBERT <mael.aubert@ekino.com>
2025-01-31 16:39:12 +0100

    incroyable convertisseur de devises

--- app/src/main/java/fr/android/eurodollar/MainActivity.java
+++ app/src/main/java/fr/android/eurodollar/MainActivity.java
@@ -1,24 +1,59 @@
 package fr.android.eurodollar;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText text;
+    Button button;
+    RadioButton euroButton;
+    RadioButton dollarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        text = (EditText) findViewById(R.id.editText1);
+        button = (Button) findViewById(R.id.button1);
+        euroButton = (RadioButton) findViewById(R.id.radio0);
+        dollarButton = (RadioButton) findViewById(R.id.radio1);
+    }
+
+    public void myClickHandler(View view) {
+        if (view.getId() == R.id.button1) {
+            if (text.getText().length() == 0) {
+                Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            float inputValue = Float.parseFloat(text.getText().toString());
+            if (euroButton.isChecked()) {
+                text.setText(String.valueOf(convertDollarToEuro(inputValue)));
+                euroButton.setChecked(false);
+                dollarButton.setChecked(true);
+            } else {
+                text.setText(String.valueOf(convertEuroToDollar(inputValue)));
+                dollarButton.setChecked(false);
+                euroButton.setChecked(true);
+            }
+        }
+    }
+
+    // Convert Dollar to Euro
+    private float convertDollarToEuro(float dollar) {
+        return (float) (dollar * 0.93); // Example conversion rate
+    }
+
+    // Convert Euro to Dollar
+    private float convertEuroToDollar(float euro) {
+        return (float) (euro * 1.07); // Example conversion rate
     }
 }
